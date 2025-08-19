b034cabe456222cf23196a769f6fcb89f93d3c57
UndrscoreEX <ethan.heikell.official@gmail.com>
2025-08-19 21:56:09 +0900

    somewhat working state. still need to figure out the spacing of sentences

---
+++ VertextAPI.py
@@ -0,0 +1,144 @@
+
+# -*- coding: utf-8 -*-
+from google.cloud import texttospeech  as tts
+# from google.cloud import texttospeech_v1 as tts # didnt work 
+# from google.cloud import texttospeech_v1beta1  as tts # didnt work
+import random
+from YTextractor import final_check_long_sentences, get_transcript, get_video_title, translate_text, split_long_sentences
+import os
+import unicodedata
+from google.cloud import storage
+
+
+PROJECT_ID=os.getenv("PROJECT_ID")
+
+def synthesize_from_file_japanese(video_code: str):
+    print(f"launching synthesis of https://www.youtube.com/watch?v={video_code}")
+    
+    # audio_output_directory = "/home/ethan/GoogleVertexAudio"
+    # os.makedirs(audio_output_directory, exist_ok=True)
+
+    text_output_directory = "/home/ethan/GoogleVertexAudio/textfiles"
+    os.makedirs(text_output_directory, exist_ok=True)
+    # gcs_output_uri_text ='gs://eng_to_japanese_podcasts_ex/Podcast_text/'
+
+    video_name = get_video_title(video_code).replace(' ', '_')
+    english_transcript = get_transcript(video_code)
+
+    original_japanese_text = translate_text(english_transcript)
+    final_japanese_text = split_long_sentences(original_japanese_text)
+
+    final_japanese_text =  unicodedata.normalize("NFC", final_japanese_text)
+    final_check_long_sentences(final_japanese_text)
+
+
+    # # 1. Save SSML to local file
+    # ssml_path = f"/tmp/{video_name}.ssml"
+    # with open(ssml_path, "w", encoding="utf-8") as f:
+    #     f.write(final_japanese_text)
+
+    # # 2. Upload SSML to GCS
+    # storage_client = storage.Client(project=PROJECT_ID)
+    # bucket = storage_client.bucket("eng_to_japanese_podcasts_ex")
+    # blob = bucket.blob(f"Podcast_text/{video_name}.ssml")
+    # blob.upload_from_filename(ssml_path)
+
+    # gcs_input_uri = f"gs://{bucket.name}/Podcast_text/{video_name}.ssml"
+        # print(final_japanese_text)
+
+    text_output_file_path = os.path.join(text_output_directory, f"{video_name}_JP.txt")
+    with open(text_output_file_path, "w", encoding="utf-8") as out_text_file:
+        out_text_file.write(original_japanese_text)
+        print(f'Translated text content written to file: {text_output_file_path}')
+    
+    gcs_output_uri_audio =f'gs://eng_to_japanese_podcasts_ex/Podcast_audio/{video_name}_JP.wav'
+    # --- Step 5: Synthesize the Japanese text into audio ---
+    client = tts.TextToSpeechLongAudioSynthesizeClient()
+    
+    voice_options = {
+        # Chirp3-HD Voices
+        "ja-JP-Chirp3-HD-Achernar": tts.SsmlVoiceGender.FEMALE,
+        "ja-JP-Chirp3-HD-Achird": tts.SsmlVoiceGender.MALE,
+        "ja-JP-Chirp3-HD-Algenib": tts.SsmlVoiceGender.MALE,
+        "ja-JP-Chirp3-HD-Algieba": tts.SsmlVoiceGender.MALE,
+        # "ja-JP-Chirp3-HD-Alnilam": tts.SsmlVoiceGender.MALE,
+        "ja-JP-Chirp3-HD-Aoede": tts.SsmlVoiceGender.FEMALE,
+        "ja-JP-Chirp3-HD-Autonoe": tts.SsmlVoiceGender.FEMALE,
+        "ja-JP-Chirp3-HD-Callirrhoe": tts.SsmlVoiceGender.FEMALE,
+        "ja-JP-Chirp3-HD-Charon": tts.SsmlVoiceGender.MALE,
+        "ja-JP-Chirp3-HD-Despina": tts.SsmlVoiceGender.FEMALE,
+        "ja-JP-Chirp3-HD-Enceladus": tts.SsmlVoiceGender.MALE,
+        "ja-JP-Chirp3-HD-Erinome": tts.SsmlVoiceGender.FEMALE,
+        "ja-JP-Chirp3-HD-Fenrir": tts.SsmlVoiceGender.MALE,
+        "ja-JP-Chirp3-HD-Gacrux": tts.SsmlVoiceGender.FEMALE,
+        "ja-JP-Chirp3-HD-Iapetus": tts.SsmlVoiceGender.MALE,
+        "ja-JP-Chirp3-HD-Kore": tts.SsmlVoiceGender.FEMALE,
+        "ja-JP-Chirp3-HD-Laomedeia": tts.SsmlVoiceGender.FEMALE,
+        "ja-JP-Chirp3-HD-Leda": tts.SsmlVoiceGender.FEMALE,
+        "ja-JP-Chirp3-HD-Orus": tts.SsmlVoiceGender.MALE,
+        "ja-JP-Chirp3-HD-Puck": tts.SsmlVoiceGender.MALE,
+        "ja-JP-Chirp3-HD-Pulcherrima": tts.SsmlVoiceGender.FEMALE,
+        "ja-JP-Chirp3-HD-Rasalgethi": tts.SsmlVoiceGender.MALE,
+        "ja-JP-Chirp3-HD-Sadachbia": tts.SsmlVoiceGender.MALE,
+        "ja-JP-Chirp3-HD-Sadaltager": tts.SsmlVoiceGender.MALE,
+        "ja-JP-Chirp3-HD-Schedar": tts.SsmlVoiceGender.MALE,
+        "ja-JP-Chirp3-HD-Sulafat": tts.SsmlVoiceGender.FEMALE,
+        "ja-JP-Chirp3-HD-Umbriel": tts.SsmlVoiceGender.MALE,
+        "ja-JP-Chirp3-HD-Vindemiatrix": tts.SsmlVoiceGender.FEMALE,
+        "ja-JP-Chirp3-HD-Zephyr": tts.SsmlVoiceGender.FEMALE,
+        "ja-JP-Chirp3-HD-Zubenelgenubi": tts.SsmlVoiceGender.MALE,
+
+        # # Neural2 Voices
+        # "Neural2-A": tts.SsmlVoiceGender.FEMALE,
+        # "Neural2-B": tts.SsmlVoiceGender.FEMALE,
+        # "Neural2-C": tts.SsmlVoiceGender.MALE,
+        # "Neural2-D": tts.SsmlVoiceGender.MALE,
+        # "AoiNeural": tts.SsmlVoiceGender.FEMALE,
+        # "DaichiNeural": tts.SsmlVoiceGender.MALE,
+        # "MayuNeural": tts.SsmlVoiceGender.FEMALE,
+        # "NaokiNeural": tts.SsmlVoiceGender.MALE,
+        # "ShioriNeural": tts.SsmlVoiceGender.FEMALE,
+
+        # # WaveNet Voices
+        # "ja-JP-Wavenet-A": tts.SsmlVoiceGender.FEMALE,
+        # "ja-JP-Wavenet-B": tts.SsmlVoiceGender.FEMALE,
+        # "ja-JP-Wavenet-C": tts.SsmlVoiceGender.MALE,
+        # "ja-JP-Wavenet-D": tts.SsmlVoiceGender.MALE,
+        }
+
+    selected_voice_name = random.choice(list(voice_options.keys()))
+    selected_gender = voice_options[selected_voice_name]
+    print(selected_voice_name, selected_gender)
+    
+
+    synthesis_input = tts.SynthesisInput(text=final_japanese_text)
+    voice = tts.VoiceSelectionParams(
+        language_code="ja-JP" ,
+        name=selected_voice_name,
+        ssml_gender=selected_gender,
+    )
+    
+    audio_config = tts.AudioConfig(audio_encoding=tts.AudioEncoding.LINEAR16)
+    
+    request = tts.SynthesizeLongAudioRequest(
+        parent=f"projects/{PROJECT_ID}/locations/us-central1",
+        input=synthesis_input,
+        # input=input_source,
+        audio_config=audio_config,
+        voice=voice,
+        output_gcs_uri=gcs_output_uri_audio,
+    )
+
+    operation = client.synthesize_long_audio(request=request)
+    try:
+        print("Waiting for long audio synthesis to complete...")
+        operation.result(timeout=300)
+        print("Synthesis completed successfully! The audio file should now be in your GCS bucket.")
+    
+    except Exception as e:
+        print(f"An error occurred during long audio synthesis: {e}")
+
+if __name__ == "__main__":
+    # videoID = input("Enter the video ID code: ")
+    # synthesize_from_file_japanese(videoID)
+    synthesize_from_file_japanese("FFIs6LsV0a4")
---
+++ YTextractor.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+from youtube_transcript_api import YouTubeTranscriptApi
+from googleapiclient.discovery import build
+from google.cloud import translate_v2 as translate
+import os
+import sys
+from dotenv import load_dotenv
+load_dotenv()
+YOUTUBE_API_KEY = os.getenv("API_KEY")
+
+def get_transcript(videoCode:int): 
+    print('Pulling the transcript')
+    ytt_api = YouTubeTranscriptApi()
+    fetched_transcript = ytt_api.fetch(videoCode)
+    full_text = " ".join([x.text for x in fetched_transcript])
+    return full_text
+
+
+def get_video_title(video_id:int):
+    print("getting video title")
+    youtube = build("youtube", "v3", developerKey=YOUTUBE_API_KEY)
+    request = youtube.videos().list(
+        part="snippet",
+        id=video_id
+    )
+    response = request.execute()
+
+    if "items" in response and response["items"]:
+        title = response["items"][0]["snippet"]["title"]
+        print('Youtube video: ',title)
+        return title
+    else:
+        return "Video not found or API error."
+    
+def translate_text(text_to_translate):
+    print('Translating to Japanese')
+    translate_client = translate.Client()
+
+    result = translate_client.translate(
+        values=text_to_translate, 
+        target_language='ja',
+        source_language='en'
+    )
+    final_result = result['translatedText'].replace(' \n',' ').replace(".","。").replace("＾","").replace("＼",'。').replace("⟪","")
+    
+    return final_result
+
+def split_long_sentences(text: str) -> str:
+    text_to_process = text
+    check_is_needed = True
+    if len(text_to_process.encode('utf-8')) <= 900:
+        return text_to_process
+
+    final_text = ""
+    while check_is_needed:
+        break_points_inserted = 0
+        all_sentences = text_to_process.split('。')
+        print('going through a loop to check all sentences')
+        
+        for sentence in all_sentences:
+            # print('this is the number of bytes',len((sentence+"。").encode('utf-8')))
+            if len((sentence+"。").encode('utf-8')) < 850:
+
+                # print('this text is fine the way it is. adding it as is' , sentence)
+                final_text+=f'{sentence}。'
+            else:
+                broken_up_text = sentence.split('、')
+                for i,chunk in enumerate(broken_up_text):
+                    
+                    if i<=len(broken_up_text)-1 and len((chunk+"。").encode('utf-8'))+ len(broken_up_text[i+1].encode('utf-8')) < 900:
+                        final_text += chunk+"、"
+
+                    elif i<=len(broken_up_text)-1 and len((chunk+"。").encode('utf-8'))+ len(broken_up_text[i+1].encode('utf-8')) >= 900:
+                        
+                        final_text += (chunk+"。")
+                        break_points_inserted +=1
+
+                    elif i==len(broken_up_text)-1:
+                        final_text += (chunk+"。")
+
+                    elif len(chunk.encode('utf-8')) > 900:
+                        print('one single chunk is too long and cant be logically split with this. this is the chunk', chunk)
+
+                    else:
+                        print('++++++++++something unforseen +++++++++++++')
+        text_to_process = final_text
+        if not break_points_inserted : 
+            check_is_needed = False 
+
+    return final_text
+        
+def final_check_long_sentences(text: str) -> None:
+    sentences = text.split('。')
+    long_sentences_found = False
+    print(sentences)
+    print(f"Checking {len(sentences)} sentences for byte length > 900...")
+    
+    for i, sentence in enumerate(sentences):
+        full_sentence = sentence + '。'
+        byte_length = len(full_sentence.encode('utf-8'))
+        
+        if byte_length > 900:
+            print("-" * 40)
+            print(f"Sentence {i+1} is too long:")
+            print(f"  Byte Length: {byte_length} bytes")
+            print(f"  Sentence: {full_sentence.strip()}")
+            long_sentences_found = True
+    
+    if not long_sentences_found:
+        print("-" * 40)
+        print("All sentences are within the byte limit.")
+    else:
+        print("-" * 40)
+        print("Please split the long sentences using the provided functions before synthesis.")
